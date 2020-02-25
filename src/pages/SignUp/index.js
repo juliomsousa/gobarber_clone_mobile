b4a8f7bab080cfb7c49bc9eeb4df8d4743dc2668
Julio Martins de Sousa <jmasousa@voegol.com.br>
2020-02-24 22:37:20 -0300

    Conectando Redux

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 import {
 	Container,
@@ -17,8 +19,16 @@ import {
 export default function SignUp({ navigation }) {
 	const emailRef = useRef();
 	const passwordRef = useRef();
+	const dispatch = useDispatch();
+	const loading = useSelector(state => state.auth.loading);
 
-	function handleSubmit() {}
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	function handleSubmit() {
+		dispatch(signUpRequest(name, email, password));
+	}
 
 	return (
 		<Background>
@@ -33,6 +43,8 @@ export default function SignUp({ navigation }) {
 						placeholder="Nome completo"
 						returnKeyType="next"
 						onSubmitEditing={() => emailRef.current.focus()}
+						value={name}
+						onChangeText={setName}
 					/>
 					<FormInput
 						icon="mail-outline"
@@ -43,6 +55,8 @@ export default function SignUp({ navigation }) {
 						ref={emailRef}
 						returnKeyType="next"
 						onSubmitEditing={() => passwordRef.current.focus()}
+						value={email}
+						onChangeText={setEmail}
 					/>
 					<FormInput
 						icon="lock-outline"
@@ -51,9 +65,13 @@ export default function SignUp({ navigation }) {
 						ref={passwordRef}
 						returnKeyType="next"
 						onSubmitEditing={handleSubmit}
+						value={password}
+						onChangeText={setPassword}
 					/>
 
-					<SubmitButton onPress={handleSubmit}>Criar conta</SubmitButton>
+					<SubmitButton loading={loading} onPress={handleSubmit}>
+						Criar conta
+					</SubmitButton>
 				</Form>
 
 				<SignLink onPress={() => navigation.navigate('SignIn')}>
