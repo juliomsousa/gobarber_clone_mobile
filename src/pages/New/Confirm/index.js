5892ad51e3f9968b28b851135000d54df6b96f6d
Julio Martins de Sousa <jmasousa@voegol.com.br>
2020-02-25 15:51:59 -0300

    Confirmando agendamento

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
 import Background from '~/components/Background';
+import api from '~/services/api';
 
-export default function Confirm() {
+export default function Confirm({ navigation }) {
+	const provider = navigation.getParam('provider');
+	const time = navigation.getParam('time');
+
+	const formattedDate = useMemo(() =>
+		formatRelative(parseISO(time), new Date(), { locale: pt })
+	);
+
+	async function handleAddAppointment() {
+		await api.post('appointments', {
+			provider_id: provider.id,
+			date: time,
+		});
+
+		navigation.navigate('Dashboard');
+	}
+
 	return (
 		<Background>
 			<Container>
-				<View />
+				<Avatar
+					source={{
+						uri: provider.avatar
+							? provider.avatar.url
+							: `https://api.adorable.io/avatar/50/${provider.name}`,
+					}}
+				/>
+				<Name>{provider.name}</Name>
+				<Time>{formattedDate}</Time>
+				<SubmitButton onPress={handleAddAppointment}>
+					Confirmar agendamento
+				</SubmitButton>
 			</Container>
 		</Background>
 	);
